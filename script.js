9e29ab2e67702d1df05d5703402508e123506322
Rachel Ramoutar <rachelramoutar1@gmail.com>
2024-05-06 23:57:06 -0400

    shaping cards and beginning to apply transformation

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-let score = 0; 
+  /*----- constants -----*/
+  const players = {
+    player1: {
+        name: "",
+        score: 0,
+    }
+}
 
-let tries = 10; 
+  /*----- state variables -----*/
+  let score = 0; 
+  let lose = numOfGuesses > MAX_GUESSES; 
+  
+
+  /*----- cached elements  -----*/
+
+
+  /*----- event listeners -----*/
+
+
+  /*----- functions -----*/
+
+let transform
