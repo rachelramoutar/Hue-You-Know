d85b68fb15cee400f72e2cca3a50388c7b1c67c4
Rachel Ramoutar <rachelramoutar1@gmail.com>
2024-05-20 21:10:25 -0400

    almost to MVP

---
+++ README.md
@@ -0,0 +1,13 @@
+#Hue You Know: 
+##Screenshots: 
+###Technologies Used:
+ *Javascript also known as ES6 or ECMAScript 2015
+ *CSS3
+ *HTML5
+
+ ###Sources: 
+ 1. https://www.w3schools.com/jsref/prop_node_textcontent.asp
+ 2. https://www.youtube.com/watch?v=bbb9dZotsOc 
+ 3. https://coolors.co/colors
+ 4. 
+ 5. Jason, the self-taught Software Engineer I met at Ash Coffee in Virginia Highlands, Atlanta, GA. 
--- script.js
+++ script.js
@@ -1,22 +1,77 @@
-  /*----- constants -----*/
-  const players = {
-    player1: {
-        name: "",
-        score: 0,
+
+const theCards = document.querySelector(".theCards");
+const totalLivesCount = document.querySelector("#totalLives");
+
+let totalLives = 9;
+let totalScore;
+let matchesMade = 0;
+let score; 
+let clickedCard = null;
+let stopClick = false;
+
+
+totalLivesCount.textContent = totalLives;
+
+function cardClicked(e) {
+  const target = e.currentTarget;
+  // console.log(target.getAttribute("data-color"));
+  if ( stopClick || target === clickedCard || target.className.includes("done")) {
+    return;
+  }
+
+  target.className = target.className.replace("color-hidden", "").trim();
+  target.className += " done";
+
+  if (!clickedCard) {
+    clickedCard = target;
+  } else if (clickedCard) {
+    if (clickedCard.getAttribute("data-color") !== target.getAttribute("data-color")
+    ) {
+      stopClick = true;
+      setTimeout(() => {
+        clickedCard.className = clickedCard.className.replace("done", "").trim() + "color-hidden";
+        target.className = target.className.replace("done", "").trim() + "color-hidden";
+        clickedCard = null;
+        stopClick = false;
+      }, 500); 
+      totalLives-=1;
+      gameOver();
+    } else {
+      // matchesMade++;
+      clickedCard = null;
+      // if (matchesMade === 8){
+      //   alert("You Win!");
+      }
     }
+  }
+// }
+
+
+function newLevel(){
+
 }
 
-  /*----- state variables -----*/
-  let score = 0; 
-  let lose = numOfGuesses > MAX_GUESSES; 
-  
-
-  /*----- cached elements  -----*/
-
-
-  /*----- event listeners -----*/
+function newGame(){
+  level=0; 
+  lives= 9; 
+  score = 0; 
+  newLevel(); 
+}
 
 
-  /*----- functions -----*/
+function matchedTotal() {
+  matchesMade++;
+  if (matchesMade === 8){
+    alert("You Win!");
+  }
+}
 
-let transform
+function gameOver(){
+  totalLives--;
+
+}
+
+
+// function flipCard(){
+//   theCards.classList.toggle("flipCard"); 
+// }
